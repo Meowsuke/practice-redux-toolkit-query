582349af7c958777afdec7fa949ce1965ee6e381
Meowsuke <58517864+Meowsuke@users.noreply.github.com>
2025-10-23 23:30:58 +0900

    first commit

---
+++ src/components/AlbumsList.js
@@ -0,0 +1,32 @@
+import { useFetchAlbumsQuery } from "../store";
+import Skeleton from "./Skeleton";
+import ExandablePanel from "./ExpandablePanel";
+
+function AlbumsList({ user }) {
+  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+
+  let content;
+  if (isLoading) {
+    content = <Skeleton time={3} />;
+  } else if (error) {
+    content = <div>Error loading albums.</div>;
+  } else {
+    content = data.map((albumn) => {
+      const header = <div>{albumn.title}</div>;
+      return (
+        <ExandablePanel key={albumn.id} header={header}>
+          List of photos in the albums
+        </ExandablePanel>
+      );
+    });
+  }
+  console.log(data, error, isLoading);
+  return (
+    <div>
+      <div>Albums For {user.name}</div>
+      <div>{content}</div>
+    </div>
+  );
+}
+
+export default AlbumsList;
